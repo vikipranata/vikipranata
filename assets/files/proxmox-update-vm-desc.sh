2cc1d9e2e9762604ebddf40f74bc89a44ed4cef7
vikipranata <helovikipranata@gmail.com>
2025-03-21 01:35:47 +0700

    Update option host-only or templates-only

--- assets/files/proxmox-update-vm-desc.sh
+++ assets/files/proxmox-update-vm-desc.sh
@@ -2,7 +2,7 @@
 log_message() {
   local message=$1
   local timestamp=$(TZ='Asia/Jakarta' date +"%Y-%m-%dT%H:%M:%S%:z")
-  echo "$timestamp - \"$message\"" | tee -a /var/log/pve/cleanup-vm-terminate.log
+  echo "$timestamp - \"$message\"" | tee -a /var/log/pve/update-vm-desc.log
 }
 
 # Define current hostname
@@ -11,8 +11,39 @@ node_hostname=$(hostname -s)
 # Initialize count of updated VMs
 updated_vms_count=0
 
-# Get VMID list retrieved from the current host
-vmid_list=$(pvesh get /nodes/$(hostname -s)/qemu --output-format json | jq -r .[].vmid)
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --host-only)
+            host_only=true
+            shift
+            ;;
+        --templates-only)
+            templates_only=true
+            shift
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+done
+
+if [ "$host_only" = true ]; then
+    vmid_list=$(pvesh get /nodes/$(hostname -s)/qemu --output-format json | jq -r .[].vmid)
+    log_message "VMID list retrieved from the current host."
+elif [ "$templates_only" = true ]; then
+    vmid_list=$(pvesh get /pools/TEMPLATES --output-format json | jq --arg node_name "$node_hostname" '.members[] | select(.node == $node_name) | .vmid')
+    log_message "VMID list retrieved from the templates pool on the current host."
+else
+    echo "Please specify one of the options: --host-only or --templates-only"
+    exit 1
+fi
+
+if [ -z "$vmid_list" ]; then
+    log_message "No VM IDs found."
+    exit 1
+fi
+
 for vmid in $vmid_list; do
     log_message "get current VM $vmid description"
     description=$(pvesh get /nodes/$node_hostname/qemu/$vmid/config --output-format json | jq -r .description)
